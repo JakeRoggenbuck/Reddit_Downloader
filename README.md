478a87f604f70559d978e1f1079b9ddd2c1d46e4
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-08-13 21:32:29 -0700

    Add comments and README instructions

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Reddit_Downloader
+
+## Get page
+Because Reddit requires authentication, it's easier to go to the website and get the page source to parse the image urls
+
+## Config
+```
+element = "classname or elecment id"
+filename = "example.txt"
+directory = "example"
+```
--- reddit_downloader.py
+++ reddit_downloader.py
@@ -11,19 +11,27 @@ class GetUrls:
         self.image_urls = []
 
     def get_file(self):
+        # Gets filename and opens file
         file1 = open(self.filename, 'r')
+        # Takes file and gets individual lines as list
         Lines = file1.readlines()
+        # Returns list of lines
         return Lines
 
     def parse_urls(self):
         Lines = self.get_file()
+        # Iterates through lines checking if element is in the line
         for line in Lines:
             if config.element in line:
+                # Gets parts of line
                 parts = line.split(' ')
                 for part in parts:
+                    # Checks for image url
                     if part[:24] == 'href="https://i.redd.it/':
                         self.raw_urls.append(part)
 
+
+    # Gets actual urls from url lines
     def extract_url(self):
         for url in self.raw_urls:
             self.image_urls.append(url[6:-1])
@@ -33,9 +41,11 @@ class Setup:
     def __init__(self, directory):
         self.directory = directory
 
+    # Check if directory exists
     def check_dir(self):
         return os.path.isdir(self.directory)
 
+    # Makes a directory if none exists
     def setup(self):
         if not self.check_dir():
             os.mkdir(self.directory)
@@ -46,10 +56,12 @@ class DownloadUrls:
         self.urls = urls
         self.path = directory
 
+    # Pases all image urls to download_image
     def download(self):
         for image in self.urls:
             self.download_image(image)
 
+    # Downloads url to directory using requests and stream
     def download_image(self, url):
         local_filename = url.split("/")[-1]
         with requests.get(url, stream=True) as r:
@@ -65,7 +77,6 @@ if __name__ == "__main__":
     DIRECTORY = config.directory
 
     geturls = GetUrls(FILENAME)
-    geturls.parse_urls()
     geturls.parse_urls()
     geturls.extract_url()
     urls = geturls.image_urls
